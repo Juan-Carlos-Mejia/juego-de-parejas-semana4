1ea7092f2c92d0e9bf4c95240f7e8691c451278b
JUAN ALFREDO MARTINEZ MOLINA <USIS001821@ugb.edu.sv>
2021-08-20 16:28:46 -0600

    Revert "messagebox"
    This reverts commit 49bb8d28de48001e466f1181c4850b9b93a91339.

--- juego de parejas/Form1.cs
+++ juego de parejas/Form1.cs
@@ -122,7 +122,7 @@ namespace juego_de_parejas
             }
 
           
-            MessageBox.Show("¡Haz encontrado todas las parejas!", "Genial");
+            MessageBox.Show("¡Haz encontrado todas las parejas!", "Fantástico");
             Close();
         }
     }
